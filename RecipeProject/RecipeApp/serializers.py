69e50dee92ceab6f6bbc79e9da3b5a46a6385623
Norbert Gniadek <norbert.gniadek@gmail.com>
2024-05-24 22:05:26 +0200

    views and serializers for Ingredient and Recipe

--- RecipeProject/RecipeApp/serializers.py
+++ RecipeProject/RecipeApp/serializers.py
@@ -1,1 +1,32 @@
 from rest_framework import serializers
+from .models import Recipe, Ingredient, RecipeIngredient
+
+class IngredientSerializer(serializers.ModelSerializer):
+    
+    calories = serializers.ReadOnlyField()
+    
+    class Meta:
+            model = Ingredient
+            fields = ['id', 'name', 'protein', 'carbohydrates', 'fat', 'calories']
+            
+
+class RecipeIngredientSerializer(serializers.ModelSerializer):
+    ingredient = IngredientSerializer()
+    
+    class Meta:
+        model = RecipeIngredient
+        fields = ['ingredient', 'quantity']
+        
+        
+class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = RecipeIngredientSerializer(source='recipeingredient_set', many=True, read_only=True)
+    total_protein = serializers.ReadOnlyField()
+    total_carbohydrates = serializers.ReadOnlyField()
+    total_fat = serializers.ReadOnlyField()
+    total_calories = serializers.ReadOnlyField()
+    
+    class Meta:
+        model = Recipefields = ['id', 'name', 'description', 'ingredients', 'total_protein', 'total_carbohydrates', 'total_fat', 'total_calories']
+        
+        
+        
--- RecipeProject/RecipeApp/views.py
+++ RecipeProject/RecipeApp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Recipe, Ingredient, RecipeIngredient
+from .serializers import RecipeSerializer, RecipeIngredientSerializer, IngredientSerializer
 
-# Create your views here.
+class RecipeList(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    
+    
+class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    
+class IngredientList(generics.ListCreateAPIView):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    
+    
+class IngredientDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    
+
