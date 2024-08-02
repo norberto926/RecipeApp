119db7a26c7f5221df6f6ffb8632ab2fd6d8d491
Norbert Gniadek <norbert.gniadek@gmail.com>
2024-08-02 13:52:31 +0200

    changing wsgi

--- RecipeProject/RecipeProject/settings.py
+++ RecipeProject/RecipeProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,17 +74,21 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'RecipeProject.wsgi.application'
+WSGI_APPLICATION = 'RecipeApp.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#      'NAME': BASE_DIR / 'db.sqlite3',
+# }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':  dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
@@ -129,5 +135,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
