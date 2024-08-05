98ea45dccf4fe0bbd173e94472778e7f85fa6c6a
Norbert Gniadek <norbert.gniadek@gmail.com>
2024-08-05 11:52:36 +0200

    allowing hosts

--- RecipeProject/RecipeProject/settings.py
+++ RecipeProject/RecipeProject/settings.py
@@ -138,6 +138,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+ALLOWED_HOSTS = ['*']
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
