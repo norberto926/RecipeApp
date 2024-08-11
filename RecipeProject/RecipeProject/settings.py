f78194c1e075d5f8088e9ec7910d11d13cf5354c
Norbert Gniadek <norbert.gniadek@gmail.com>
2024-08-11 11:22:57 +0200

    change bucket

--- RecipeProject/RecipeProject/settings.py
+++ RecipeProject/RecipeProject/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = 'recipe-app-static-ng2-2'
+AWS_STORAGE_BUCKET_NAME = 'recipe-app-static-ng2'
 AWS_S3_SIGNATURE_NAME = 's3v4',
 AWS_S3_REGION_NAME = 'eu-central-1'
 AWS_S3_FILE_OVERWRITE = False
