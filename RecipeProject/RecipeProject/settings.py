9dd4a111ed8dc070d439e109e921e36d64e5926a
Norbert Gniadek <norbert.gniadek@gmail.com>
2024-08-11 09:36:41 +0200

    again

--- RecipeProject/RecipeProject/settings.py
+++ RecipeProject/RecipeProject/settings.py
@@ -149,5 +149,5 @@ AWS_DEFAULT_ACL =  None
 AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/photos/'
+MEDIA_ROOT = 'photos/'
